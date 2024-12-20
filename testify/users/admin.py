26de3de22d018ffb65071081c6588004916e1c5e
Oleh <oleh.lohvyn@gmail.com>
2024-12-21 01:36:14 +0200

    Add admin integration for CustomUser model

--- testify/users/admin.py
+++ testify/users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email'),
+        }),
+    )
+
+    ordering = ('username',)
--- testify/users/models.py
+++ testify/users/models.py
@@ -3,5 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    # Тут можна додавати додаткові поля
     pass
